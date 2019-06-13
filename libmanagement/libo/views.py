df99ed80b7ac0dce5a5b13dff9bac176de4b0620
JaredAhaza <jaredahazq@gmail.com>
2019-06-13 16:22:48 +0300

    Made various changes and tested the functionality of urls

--- libmanagement/libo/views.py
+++ libmanagement/libo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, we welcome you to our services that allows you to have better library management services.")
