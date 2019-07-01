c9e788e1f3effb05bd8176c0b60a372b05232c13
JaredAhaza <jaredahazq@gmail.com>
2019-07-01 12:50:40 +0300

    registred models

--- libmanagement/libo/admin.py
+++ libmanagement/libo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
+
+admin.site.register(Author)
 # Register your models here.
+# admin.site.register(Publisher)
+# admin.site.register(LendPeriods)
+# admin.site.register(Book)
+# admin.site.register(UserProfile)
+# admin.site.register(Review)
