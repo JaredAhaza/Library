60b6296f74fc7340017015743c4e95b190407a22
JaredAhaza <jaredahazq@gmail.com>
2019-07-01 11:58:32 +0300

    def was_published_recently(self):

--- libmanagement/libo/models.py
+++ libmanagement/libo/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
+from django.utils import timezone
+
 
 # Create your models here.
 class Book(models.Model):
@@ -13,8 +16,14 @@ class Book(models.Model):
 	lend_by = models.ForeignKey('UserProfile', null=True, blank=True, on_delete=models.CASCADE)
 	lend_date = models.DateField(null=True, blank=True)
 	
-
-
+	
+	def __unicode__(self):
+		return 'Book: ' + self.title
+	
+	class Meta:
+		ordering = ['title']
+		verbose_name = "Book"
+		verbose_name_plural = "Books"
 
 
 
@@ -26,6 +35,15 @@ class LendPeriods(models.Model):
 	"""
 	name = models.CharField(max_length=30)
 	days_amount = models.IntegerField()
+	
+	def __unicode__(self):
+		return '%s' % self.name
+	
+	class Meta:
+		get_latest_by = "days_amount"
+		ordering = ['days_amount']
+		verbose_name = "Lending period"
+		verbose_name_plural = "Lending periods"
 
 
 
@@ -34,9 +52,15 @@ class Publisher(models.Model):
 	Class defines book's publisher
 	"""
 	name = models.CharField(max_length=30)
-
-
-
+	
+	def __unicode__(self):
+		return 'Publisher: %s' % self.name
+		
+	class Meta:
+		get_latest_by = "name"
+		ordering = ['name']
+		verbose_name = "Publisher"
+		verbose_name_plural = "Publishers"
 
 
 class Author(models.Model):
@@ -46,6 +70,19 @@ class Author(models.Model):
 	name = models.CharField(max_length=30)
 	surname = models.CharField(max_length=30)
 	date_of_birth = models.DateField()
+	
+	def __unicode__(self):
+		return 'Author: ' + self.name + ' ' + self.surname
+		
+	def __str__(self):
+		return 'Author: ' + self.name + ' ' + self.surname
+	
+	class Meta:
+		get_latest_by = "name"
+		ordering = ['name', 'surname']
+		verbose_name = "Author"
+		verbose_name_plural = "Authors"
+
 
 
 
@@ -60,7 +97,20 @@ class review(models.Model):
 	review = models.CharField(max_length=600, null=False, blank=False)
 	pub_date = models.DateTimeField('date published')
 
+def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+ def __unicode__(self):
+        return 'review: %s...' % self.quotation[0:12]
+
+    def get_full_review(self):
+        return '%s' % self.review
+
+    class Meta:
+        get_latest_by = "creation_date"
+        ordering = ['review']
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
 
 
 
@@ -74,3 +124,31 @@ class UserProfile(models.Model):
 	fb_name = models.CharField(max_length=60, null=True, blank=True)
 	friends = models.ManyToManyField('self', symmetrical=True)
 	join_date = models.DateField()
+	
+	def __unicode__(self):
+		return 'User profile: ' + self.user.username + ', ' + self.user.first_name + ' ' + self.user.last_name
+		
+	def gravator_url(self):
+		return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
+		
+	class Meta:
+		get_latest_by = "join_date"
+		ordering = ['user']
+		verbose_name = "User profile"
+		verbose_name_plural = "User profiles"
+
+
+
+def get_or_create_userprofile(user):
+	if user:
+		# up = get_object_or_404(UserProfile, user=user)
+		try:
+			up = UserProfile.objects.get(user=user)
+			if up:
+				return up
+			except ObjectDoesNotExist:
+			pass
+	up = UserProfile(user=user, join_date=timezone.now())
+	up.save()
+	return up
+User.profile = property(lambda u: get_or_create_userprofile(user=u))
